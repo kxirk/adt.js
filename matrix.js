5d3de8a56aa12b49cf967bb3fd8f67e79a2273f7
Cody Morton <32003959+kxirk@users.noreply.github.com>
2023-08-07 15:47:12 -0500

    add matrix adt

---
+++ matrix.js
@@ -0,0 +1,62 @@
+/**
+ * @typedef pos
+ * @type {number[]} [y, x]
+ */
+
+const Matrix = class extends Array {
+  /**
+   * @param {number} [height]
+   * @param {number} [width]
+   */
+  constructor (height = 0, width = 0) {
+    super(height);
+
+    this.height = height;
+    this.width = width;
+  }
+
+
+  /** @type {number} */
+  get height () { return this.length; }
+  set height (height) {
+    this.length = height;
+    for (let y = 0; y < height; y++) {
+      this[y] ??= [];
+      this[y].length = this.width;
+    }
+  }
+
+  /** @type {number} */
+  get width () { return (this[0]?.length ?? 0); }
+  set width (width) {
+    for (let y = 0; y < this.height; y++) {
+      this[y].length = width;
+    }
+  }
+
+
+  /**
+   * @param {number} y
+   * @param {number} x
+   * @returns {*}
+   */
+  at (y, x) {
+    return super.at(y).at(x);
+  }
+
+  /**
+   * @param {*} value
+   * @param {pos} [start]
+   * @param {pos} [end]
+   * @returns {Matrix}
+   */
+  fill (value, start = [0, 0], end = [this.height, this.width]) {
+    for (let y = start[0]; y < end[0]; y++) {
+      this[y].fill(value, start[1], end[1]);
+    }
+    return this;
+  }
+};
+
+
+export default Matrix;
