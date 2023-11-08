7fc521e8e3f4620331dfd00878b5d94d9279fa14
Cody Morton <32003959+kxirk@users.noreply.github.com>
2023-11-07 20:10:34 -0600

    rebase interval -> range

---
+++ range.js
@@ -0,0 +1,61 @@
+const Range = class {
+  /** @type {number} */
+  #min;
+  /** @type {number} */
+  #max;
+  /** @type {number} */
+  #step;
+
+  /**
+   * @param {number} min
+   * @param {number} [max]
+   * @param {number} [step]
+   */
+  constructor (min, max = Infinity, step = 1) {
+    this.min = min;
+    this.max = max;
+    this.step = step;
+  }
+
+
+  /** @type {number} */
+  get min () { return this.#min; }
+  set min (min) { this.#min = min; }
+
+  /** @type {number} */
+  get max () { return this.#max; }
+  set max (max) { this.#max = max; }
+
+  /** @type {number} */
+  get step () { return this.#step; }
+  set step (step) { this.#step = step; }
+
+
+  /**
+   * @param {number} number
+   * @returns {boolean}
+   */
+  includes (number) {
+    return (number >= this.min) && (number < this.max);
+  }
+
+
+  /** @type {Function} */
+  [Symbol.iterator] () {
+    let value = this.min;
+
+    return {
+      next: () => {
+        if (value < this.max) {
+          const result = { value, done: false };
+          value += this.step;
+
+          return result;
+        }
+
+        return { value, done: true };
+      }
+    };
+  }
+};
+export default Range;
