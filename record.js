806a410ddff29460d2b5e9864c095bc3047ea9b8
Cody Morton <32003959+kxirk@users.noreply.github.com>
2023-11-07 20:13:38 -0600

    emulate map built-in

--- record.js
+++ record.js
@@ -3,7 +3,44 @@ const Record = class {
    * @param {Object} [object]
    */
   constructor (object = {}) {
-    return Object.setPrototypeOf(Object.assign(object), null);
+    const record = Object.setPrototypeOf(Object.assign(object), null);
+
+    Object.defineProperty(record, "size", {
+      get () {
+        return this.keys().length;
+      },
+      enumerable: false
+    });
+
+    Object.defineProperty(record, "entries", {
+      value () {
+        return Object.entries(this);
+      },
+      enumerable: false
+    });
+
+    Object.defineProperty(record, "has", {
+      value (key) {
+        return (this[key] !== undefined);
+      },
+      enumerable: false
+    });
+
+    Object.defineProperty(record, "keys", {
+      value () {
+        return Object.keys(this);
+      },
+      enumerable: false
+    });
+
+    Object.defineProperty(record, "values", {
+      value () {
+        return Object.values(this);
+      },
+      enumerable: false
+    });
+
+    return record;
   }
 };
 export default Record;
