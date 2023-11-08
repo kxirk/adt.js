46d7c0f0d392f008578e8ba5838cb660e9ed5f43
Cody Morton <32003959+kxirk@users.noreply.github.com>
2023-11-07 20:29:50 -0600

    add package

---
+++ index.js
@@ -0,0 +1,7 @@
+export { default as FrequencyMap } from "./frequency-map.js";
+export { default as Heap } from "./heap.js";
+export { default as Matrix } from "./matrix.js";
+export { default as PriorityQueue } from "./priority-queue.js";
+export { default as Range } from "./range.js";
+export { default as Record } from "./record.js";
+export { default as TokenBucket } from "./token-bucket.js";
